2bd2a88b6d6642c8c828b9bbde09594f93e75636
Shaun Assam <thebeardedhermit@gmail.com>
2026-01-05 17:06:44 -0500

    Update 04-flatpaks.sh

--- build_files/base/04-flatpaks.sh
+++ build_files/base/04-flatpaks.sh
@@ -7,6 +7,4 @@ org.mozilla.Thunderbird/x86_64/stable \
 com.mattjakeman.ExtensionManager \
 com.ranfdev.DistroShelf/x86_64/stable \
 app/org.gnome.Firmware/x86_64/stable \
-app/com.github.tchx84.Flatseal/x86_64/stable \
-runtime/org.gtk.Gtk3theme.adw-gtk3 \
-runtime/org.gtk.Gtk3theme.adw-gtk3-dark
+app/com.github.tchx84.Flatseal/x86_64/stable
